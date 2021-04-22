3ceeaf9fed45b52fabc0b91c255dc778555e4676
Lucas Costa <lucas.costa@cjr.org.br>
2021-04-22 15:06:01 -0300

    feat: final signUp page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+
 import Login from "./pages/login/login" // Não vai ficar desse jeito
+import SignUp from "./pages/signUp/signUp" // Não vai ficar desse jeito
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <SignUp/>
       {/* Não vai ficar desse jeito, tem que botar rotas */}
     </div>
   );
---
+++ src/pages/signUp/signUp.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react"
+import axios from "axios"
+
+import "./signUp.css"
+
+import Input from "../../components/Input/Input"
+import Button from "../../components/Button/Button"
+
+export default function Login() {
+    const [err1, setErr1] = useState('');
+    const [err2, setErr2] = useState('');
+    const login = (event) => {
+        event.preventDefault();
+        const [email, password, password_confirmation] = Array.from(event.target.elements).map(
+            (el) => el.value
+        )
+        console.log(email, password, password_confirmation);
+        axios
+            .post("http://localhost:3001/users", {user:{email, password, password_confirmation}})
+            .then(response => {
+                console.log(response)
+            })
+            .catch(err =>{
+                console.log(err.response);
+                if(err.response.data.email){
+                    setErr1(err.response.data.email[0]);
+                    console.log(err1)
+                }
+                if(err.response.data.password){
+                    setErr2(err.response.data.password[0]);
+                    console.log(err.response)
+                }
+            })
+    }
+
+    return(
+        <>
+        <div className = "container">
+            <form className = "form--signUp" onSubmit={login}>
+                <Input name= "email" id="email" type="email" text="Email" err={err1}/>
+                <Input name= "password" id="password" type="password" text="Senha"/>
+                <Input name= "password_confirmation" id="password_confirmation" type="password" text="Confirmar senha"/>
+                <Button text = "cadastrar" type= "submit"/>
+            </form>
+        </div>
+        </>
+    )
+}
