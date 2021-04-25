0640b36a9dfb01a9befcd6d6221e98088498b9e5
pedrohmoura <pedrohmoura19@gmail.com>
2021-04-25 19:51:51 -0300

    Routes changed

---
+++ src/components/SurveyAnswer/SurveyAnswer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'SurveyAnswer.css';
+
+class SurveyAnswer extends React.Components {
+    render() {
+        return(
+            <div className="SurveyAnswer">
+                
+            </div>
+        );
+    }
+}
