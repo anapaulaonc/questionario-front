cc97a3ba3726356c1090bd6cc7cec1cf7ef76904
pedrohmoura <pedrohmoura19@gmail.com>
2021-04-25 19:52:11 -0300

    Routes changed

--- src/Route.js
+++ src/Route.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 import Login from './pages/login/login';
 import ForYou from './pages/foryou/foryou';
+import SignUp from './pages/signUp/signUp'
 
 // Usar comando << npm install react-router-dom >> para instalar o pacote de rotas.
 
@@ -10,6 +11,9 @@ const Routes = () => (
         <Switch>
             <Route exact path = "/login">
                 <Login/>
+            </Route>
+            <Route exact path = "/signUp">
+                <SignUp/>
             </Route>
             <Route exact path = "/">
                 <ForYou/>
