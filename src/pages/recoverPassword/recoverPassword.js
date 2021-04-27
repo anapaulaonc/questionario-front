06e081706259695f31f5ec7a860523b27851f1d1
Lucas Costa <lucas.costa@cjr.org.br>
2021-04-27 15:33:44 -0300

    feat: add recover page

--- src/pages/recoverPassword/recoverPassword.js
+++ src/pages/recoverPassword/recoverPassword.js
@@ -7,11 +7,25 @@ import Input from "../../components/Input/Input"
 import Button from "../../components/Button/Button"
 
 export default function RecoverPassword() {
+    const login = (event) => {
+        event.preventDefault();
+        const [email, password, password_confirmation] = Array.from(event.target.elements).map(
+            (el) => el.value
+        )
+        console.log("Email: ", email);
+        console.log("Nova senha: ", password);
+        console.log("Confirmação da nova senha: ", password_confirmation);
+
+    }
     
     return(
         <>
-        <div className = "container">
-            <h1>Teste</h1>
+        <div className = "container--recover">
+            <form>
+            <Input name= "email" id="email" type="email" placeholder="Email"/>
+            <Input name= "password" id="password" type="password" placeholder="Nova senha"/>
+            <Input name= "password_confirmation" id="password_confirmation" type="password" text="NOva senha"/>
+            </form>
         </div>
         </>
     )
