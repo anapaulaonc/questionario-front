8324ad043c49f9ae989eabc6986a4124936b1fc8
anapaulaonc <anapaulaoncosta@gmail.com>
2021-05-16 17:48:46 -0300

    pagina foryou

--- src/pages/foryou/foryou.js
+++ src/pages/foryou/foryou.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
-import SurveyForm from "../../components/Survey/SurveyForm";
 
-class ForYou extends React.Component {
-  render() {
-    return (
-      <div className="ForYou">
-        <NavBar />
-      </div>
-    );
-  }
+function ForYou() {
+  const [surveys, setSurveys] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/surveys`)
+      .then((res) => {
+        console.log(res.data);
+        setSurveys(res.data);
+      })
+      .catch(() => {
+        console.log("ERRAAAADO dentro do catch");
+      });
+  }, []);
+  let choosepath = useHistory();
+
+  return (
+    <div className="ForYou">
+      <NavBar />
+      {surveys.map((s) => {
+        return (
+          <div
+            key={s.id}
+            onClick={() => {
+              choosepath.push(`/questionario/${s.id}`);
+            }}
+          >
+            <h3>{s.title}</h3>
+            <h5>por: {s.user.email}</h5>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default ForYou;
